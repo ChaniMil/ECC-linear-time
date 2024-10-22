90e090f6793a5c40cf897823cea066bd15fd5b2e
ChaniMil <135542653+ChaniMil@users.noreply.github.com>
2024-10-22 19:58:58 +0300

    Add files via upload

--- rs_exp.py
+++ rs_exp.py
@@ -1,41 +1,53 @@
-import string
-import random
 import time
 from reedsolo import RSCodec
-from experiments_linear_time import you_choose_k_exclamation_mark
+from parameters import choose_params_by_code_dimension
 from main_code import init_graphs, linear_encode, linear_decode
+
+CODE_DIMENSION = 80808
+C_EXP = 17  # block size for RS - 2^c_exp - 1
 
 
 def main():
-    r, epsilon = (0.7971014492753623, 0.14545454545454545)
-    (k, pr, qr, pe, qe, b, b_padding, rate_eff) = you_choose_k_exclamation_mark(epsilon=epsilon, r=r)[0]
-    print(k, pr, qr, pe, qe, b, b_padding, rate_eff)
-    gr, ge = init_graphs(pr, qr, pe, qe)
-    characters = string.printable  # Includes all printable characters
-    random_string = ''.join(random.choice(characters) for _ in range(k))
-    params = (random_string.encode(), pr, qr, pe, qe, b, epsilon, ge, gr)
+    # parameters
+    pr, pe, q, b, r, epsilon, k = choose_params_by_code_dimension(CODE_DIMENSION)
+    qr = qe = q
+    print(pr, qr, pe, qe, b, r, epsilon, k)
+
+    msg_to_encode = 'A' * k
+
+    # LT setup
+    gr, ge = init_graphs(pr, qr, pe, qe)  # graphs
+    params = (msg_to_encode.encode(), pr, qr, pe, qe, b, epsilon, gr, ge)
+
+    # LT encoding
     enc_start_time = time.time()
     encoded_message = linear_encode(*params)
     enc_stop_time = time.time()
-    dec_start_time = time.time()
-    decoded_message = linear_decode(encoded_message, *params[1:])
-    dec_stop_time = time.time()
     print("LT Encoding time: ", enc_stop_time - enc_start_time)
-    print("LT Decoding time: ", dec_stop_time - dec_start_time)
-    if decoded_message == random_string.encode():
-        print('Successful LT decoding')
 
-    rs = RSCodec(int(k*(1-r)/r), nsize=32639)
+    # LT decoding
+    dec_start_time = time.time()
+    linear_decode(encoded_message, *params[1:])
+    dec_stop_time = time.time()
+    print("LT Decoding time: ", dec_stop_time - dec_start_time)
+
+    # RS setup
+    rate = r  # can be r + epsilon, for different comparison
+    redundancy_length = int(k / rate) - k
+    msg_to_encode = 'A' * k
+    rs = RSCodec(redundancy_length, c_exp=C_EXP)
+
+    # RS encoding
     enc_start_time = time.time()
-    enc_msg = rs.encode(random_string.encode())
+    enc_msg = rs.encode(msg_to_encode.encode('utf-8'))  # Encode as bytes
     enc_stop_time = time.time()
     print("RS Encoding time: ", enc_stop_time - enc_start_time)
+
+    # RS decoding
     dec_start_time = time.time()
-    dec_msg = rs.decode(enc_msg)[0]
+    rs.decode(enc_msg)
     dec_stop_time = time.time()
     print("RS Decoding time: ", dec_stop_time - dec_start_time)
-    if enc_msg == dec_msg:
-        print('Successful RS decoding')
 
 
 if __name__ == '__main__':
